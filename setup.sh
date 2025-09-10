8002e2d1d563d15c04080027acc329b7541b8471
san-labs21 <01sandhy@gmail.com>
2025-09-10 16:27:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,37 @@ cd /opt/
 mkdir -p katalog
 cd katalog
 python3 -m venv bot
-source bot/bin/activate
+wget -q https://raw.githubusercontent.com/san-labs21/katalog/main/admin.html
+wget -q https://raw.githubusercontent.com/san-labs21/katalog/main/katalog.html
+wget -q https://raw.githubusercontent.com/san-labs21/katalog/main/app.py
 
+cat <<EOL > /opt/katalog/run.sh
+#!/bin/bash
+source /opt/bot/bin/activate
+python3 /opt/katalog/app.py
+EOL
+
+# Buat file service systemd
+cat <<EOF > /etc/systemd/system/katalog.service
+[Unit]
+Description=San Bot Manager
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/bash /opt/katalog/run.sh
+WorkingDirectory=/opt/katalog
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd dan mulai service
+systemctl daemon-reload
+systemctl enable katalog
+systemctl start katalog
+ 
  
